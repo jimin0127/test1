77bcbee6e5b53ecefc2ffa12ca3594e69d262557
jimin0127 <ckdgh4662@naver.com>
2022-03-17 01:29:48 +0900

    first commit

---
+++ src/main/kotlin/com/example/test1/Test1Application.kt
@@ -0,0 +1,11 @@
+package com.example.test1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Test1Application
+
+fun main(args: Array<String>) {
+    runApplication<Test1Application>(*args)
+}
